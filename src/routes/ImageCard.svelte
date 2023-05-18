f5e56745090304d8ec48e27703888f9f5c01f16f
ShadowAlzazel <pastenalfredojr@gmail.com>
2023-05-17 23:15:24 -0700

    First

---
+++ src/routes/ImageCard.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	export let imageSrc: string;
+	export let imageTitle: string;
+	export let imageText: string;
+</script>
+
+<div class="image-card" draggable="false">
+	<img class="image" src={imageSrc} alt="" draggable="false" />
+	<div class="image-details">
+		<h3 class="image-title">{imageTitle}</h3>
+		<p class="image-text">{imageText}</p>
+	</div>
+</div>
+
+<style>
+	.image-card:hover .image-details {
+		top: 50%;
+		left: 50%;
+		opacity: 1;
+	}
+
+	.image {
+		width: 40vmin;
+		height: 62vmin;
+		object-fit: cover;
+		object-position: center;
+	}
+
+	.image-details {
+		bottom: 0;
+		text-align: center;
+		padding-left: 1em;
+		padding-right: 1em;
+		width: 100%;
+		opacity: 0;
+		transition: opacity 350ms ease;
+	}
+
+	.image-details h3 {
+		color: rgb(0, 0, 0);
+		font-weight: 500;
+		font-size: x-large;
+		font-family: monospace;
+		letter-spacing: 0.15em;
+		margin-bottom: 0.2em;
+	}
+
+	.image-details p {
+		color: rgb(3, 12, 19);
+		font-size: large;
+		font-family: monospace;
+	}
+</style>
---
+++ src/routes/ImageTrack.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+	import ImageCard from './ImageCard.svelte';
+
+	import image_fallback from '$lib/images/IMG_3004.jpg';
+	const imageTitle = 'Ionic Scroll';
+	const imageText = 'The ionic scroll is cool';
+
+	let track: HTMLElement;
+
+	const handleMouseDown = (event: MouseEvent): void => {
+		track.dataset.mouseDownAt = event.clientX.toString();
+	};
+
+	const handleMouseUp = (): void => {
+		track.dataset.mouseDownAt = '0';
+		track.dataset.prevPercentage = track.dataset.percentage ?? '';
+	};
+
+	const handleMouseMove = (event: MouseEvent): void => {
+		if (track.dataset.mouseDownAt === '0') return;
+
+		const mouseDelta = parseFloat(track.dataset.mouseDownAt ?? '0') - event.clientX;
+		const maxDelta = window.innerWidth / 0.5;
+
+		const percentage = (mouseDelta / maxDelta) * -100;
+		const nextPercentageRaw = parseFloat(track.dataset.prevPercentage ?? '0') + percentage;
+		const nextPercentage = Math.max(Math.min(nextPercentageRaw, 0), -100);
+
+		track.dataset.percentage = nextPercentage.toString();
+
+		track.animate(
+			{ transform: `translate(${nextPercentage}%, -50%)` },
+			{ duration: 1200, fill: 'forwards' }
+		);
+
+		const images = track.getElementsByClassName('image');
+		for (let i = 0; i < images.length; i++) {
+			const image = images[i] as HTMLElement;
+			image.animate(
+				{ objectPosition: `${100 + nextPercentage}% center` },
+				{ duration: 1200, fill: 'forwards' }
+			);
+		}
+	};
+</script>
+
+<div
+	class="image-track"
+	draggable="false"
+	data-mouse-down-at="0"
+	data-prev-percentage="0"
+	on:mousemove={handleMouseMove}
+	on:mousedown={handleMouseDown}
+	on:mouseup={handleMouseUp}
+	bind:this={track}
+>
+	<ImageCard imageSrc={image_fallback} {imageTitle} {imageText} />
+    <ImageCard imageSrc={image_fallback} {imageTitle} {imageText} />
+    <ImageCard imageSrc={image_fallback} {imageTitle} {imageText} />
+    <ImageCard imageSrc={image_fallback} {imageTitle} {imageText} />
+    <ImageCard imageSrc={image_fallback} {imageTitle} {imageText} />
+</div>
+
+<style>
+	.image-track {
+    display: flex;
+    gap: 4vmin;
+    position: absolute; 
+    left: 50%;
+    top: 50%;
+    transform: translate(0%, -50%);
+    transition: opacity 400ms ease;
+}
+
+</style>
